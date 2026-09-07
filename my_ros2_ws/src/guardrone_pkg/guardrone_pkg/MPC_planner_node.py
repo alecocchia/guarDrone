51b3dca0bdf417d140664b34e6e9d3a8961883f8
Alessandro Cocchia <alexcocchia98@gmail.com>
2026-09-07 18:30:45 +0200

    modifica peso Q_e

--- my_ros2_ws/src/guardrone_pkg/guardrone_pkg/MPC_planner_node.py
+++ my_ros2_ws/src/guardrone_pkg/guardrone_pkg/MPC_planner_node.py
@@ -660,7 +660,7 @@ class MpcPlannerNode(Node):
 
         R   = ca.diagcat(R_f, R_tau)
         Q   = ca.diagcat(5*Q_cyl, Q_vel, Q_ang_dot, Q_acc, Q_acc_ang)
-        Q_e = ca.diagcat(5 * Q_cyl, 10*Q_vel, 5.5*Q_ang_dot,2*Q_acc, 2*Q_acc_ang)
+        Q_e = ca.diagcat(5 * Q_cyl, 8*Q_vel, 5.5*Q_ang_dot,2*Q_acc, 2*Q_acc_ang)
 
 
         u_min = np.array([0.0, -self.U_TAU_X, -self.U_TAU_Y, -self.U_TAU_Z])
