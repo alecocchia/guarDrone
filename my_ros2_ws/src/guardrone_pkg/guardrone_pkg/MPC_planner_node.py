dbabf1903e0caaf5504d698403b4b4957cfe51fa
Alessandro Cocchia <alexcocchia98@gmail.com>
2026-09-07 16:56:04 +0200

    modifica peso yaw

--- my_ros2_ws/src/guardrone_pkg/guardrone_pkg/MPC_planner_node.py
+++ my_ros2_ws/src/guardrone_pkg/guardrone_pkg/MPC_planner_node.py
@@ -618,9 +618,9 @@ class MpcPlannerNode(Node):
         E_INT_CART = np.array([1, 1, 1])
 
         V       = np.array([0.2, 0.2, 0.3]) 
-        ANG_DOT = np.array([0.15, 0.15, 0.35]) 
-        ACC     = np.array([0.4, 0.4, 0.4])/2 ## OK ANCHE DIVIDENDO PER 2 (CON ESTIMATOR)
-        ACC_ANG = np.array([0.3, 0.3, 0.4])
+        ANG_DOT = np.array([0.15, 0.15, 0.25]) 
+        ACC     = np.array([0.3, 0.3, 0.4])/2 ## OK ANCHE DIVIDENDO PER 2 (CON ESTIMATOR)
+        ACC_ANG = np.array([0.3, 0.3, 0.35])
         JERK    = 20.0
         SNAP    = 200.0
 
